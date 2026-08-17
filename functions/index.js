8604a15b3ba8f145f8b1b40a701e45fa5364e9c6
will <lecarnetduchef@gmail.com>
2026-08-17 21:07:47 +0200

    fix: restrict createPayment to POST

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,15 @@ const { createPayment, CreatePaymentError } = require("./createPayment");
 const createPaymentHttp = onRequest(
   { region: "europe-west9" },
   async (req, res) => {
+    if (req.method !== "POST") {
+      res.set("Allow", "POST");
+      return res.status(405).json({
+        ok: false,
+        code: "METHOD_NOT_ALLOWED",
+        message: "Method Not Allowed",
+      });
+    }
+
     try {
       const result = await createPayment(req.body);
       return res.status(200).json(result);
